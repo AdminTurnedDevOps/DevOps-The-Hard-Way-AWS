f3bf654f41652ce1f97292f7a522f7062b0240be
Michael Levan <mlevan1992@gmail.com>
2021-06-10 10:10:35 -0400

    host and dockerfile change

--- Docker/app/app.py
+++ Docker/app/app.py
@@ -230,4 +230,4 @@ def get_redirect_uri(request):
 
 if __name__ == '__main__':
     app.debug = os.environ.get('FLASK_DEBUG', True)
-    app.run(host='127.0.0.1', port=5000)
+    app.run(host='0.0.0.0', port=5000)
