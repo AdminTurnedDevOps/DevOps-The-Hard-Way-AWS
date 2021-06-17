163e9c16cd4e27caad53311ca165781a09d42e08
Michael Levan <mlevan1992@gmail.com>
2021-06-17 10:53:31 -0400

    eks fargate

--- Terraform-AWS-Services-Creation/EKS/main.tf
+++ Terraform-AWS-Services-Creation/EKS/main.tf
@@ -12,7 +12,7 @@ terraform {
 }
 
 
-## IAM Role for EKS to have access to the appropriate resources
+# IAM Role for EKS to have access to the appropriate resources
 resource "aws_iam_role" "eks-iam-role" {
   name = "devopsthehardway-eks-iam-role"
 
@@ -55,52 +55,34 @@ resource "aws_eks_cluster" "devopsthehardway-eks" {
   ]
 }
 
-resource "aws_iam_role" "devopsthehardway-iam-nodegroup" {
-  name = "devopsthehardway-nodegroup"
+
+resource "aws_iam_role" "eks-fargate" {
+  name = "eks-fargate-devopsthehardway"
 
   assume_role_policy = jsonencode({
     Statement = [{
       Action = "sts:AssumeRole"
       Effect = "Allow"
       Principal = {
-        Service = "ec2.amazonaws.com"
+        Service = "eks-fargate-pods.amazonaws.com"
       }
     }]
     Version = "2012-10-17"
   })
 }
 
-resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.devopsthehardway-iam-nodegroup.name
+resource "aws_iam_role_policy_attachment" "AmazonEKSFargatePodExecutionRolePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+  role       = aws_iam_role.eks-fargate.name
 }
 
-resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.devopsthehardway-iam-nodegroup.name
-}
+resource "aws_eks_fargate_profile" "devopsthehardway-eks-serverless" {
+  cluster_name           = aws_eks_cluster.devopsthehardway-eks.name
+  fargate_profile_name   = "devopsthehardway-serverless-eks"
+  pod_execution_role_arn = aws_iam_role.eks-fargate.arn
+  subnet_ids             = [var.private_subnet_id_1]
 
-resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.devopsthehardway-iam-nodegroup.name
-}
-
-resource "aws_eks_node_group" "devopsthehardway-eks" {
-  cluster_name    = aws_eks_cluster.devopsthehardway-eks.name
-  node_group_name = "devopsthehardway-nodegroup"
-  node_role_arn   = aws_iam_role.devopsthehardway-iam-nodegroup.arn
-  subnet_ids      = [var.subnet_id_1, var.subnet_id_2]
-
-  scaling_config {
-    desired_size = 3
-    max_size     = 3
-    min_size     = 1
+  selector {
+    namespace = "default"
   }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
-    aws_eks_cluster.devopsthehardway-eks,
-  ]
 }
